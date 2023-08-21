cd9d799630ebdd5e9ce83e05596e7ec82ef37166
Tran Ngoc Nhan <nhantran20072001@gmail.com>
2023-08-21 13:11:21 +0700

    Học print và input trong lập trình python

---
+++ input.py
@@ -0,0 +1,10 @@
+input("Nhập vào một con số: ")
+
+a = input("Nhập vào a: ")
+
+print("a = {0}".format(a))
+
+ho = input ("Nhap họ: ")
+ten = input("Nhập tên: ")
+
+print("Xin chào", ho, ten)
---
+++ print.py
@@ -0,0 +1,16 @@
+# Các loại print
+
+print()
+
+print(5)
+
+print(7);print("helle world") # Dấu ; để ngăn cách giúp hệ thống hiểu là 2 dòng lệnh
+
+print("hello","world")
+
+print("xin","chào",2007,sep="\n") # Ngăn cách các kết quả trong câu lệnh bằng \n
+
+print("xin","chào",end=":")       # Thay cho việc xuống dòng khi kết thúc câu lệnh bằng dấu :
+print(2023)
+
+print('Tên={0}, Họ={1}'.format('Nhân', 'Trần')) # Truyền dữ liệu vào biến 0, 1 thông qua format
